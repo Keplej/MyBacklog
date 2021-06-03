3894dbe2192b03c75be8e4fb29c4c9c81597bc48
Johnny Keple <Johnny.Keple@gmail.com>
2021-06-03 10:48:39 -0500

    Able to view a single game in current

--- src/redux/sagas/currentlist.saga.js
+++ src/redux/sagas/currentlist.saga.js
@@ -1,11 +1,11 @@
-import { takeLatest, put } from "@redux-saga/core/effects";
+import { takeEvery, put } from "@redux-saga/core/effects";
 import axios from "axios";
 
 
 function* fetchList() {
     try {
         const response = yield axios.get('/api/current');
-        yield put ({type: 'SET_LIST', payload: response})
+        yield put ({type: 'SET_CURRENT', payload: response.data})
     } catch (error) {
         alert(`Sorry cannot fetchList atht the moment`);
         console.log('Error in getting List', error);
@@ -16,7 +16,7 @@ function* deleteCurrentGame(action) {
     try {
         let id = action.payload;
         yield axios.delete(`/api/current/${id}`);
-        yield put({type: 'GET_GAMES'});
+        yield put({type: 'FETCH_CURRENT'});
     } catch (error) {
         alert(`Error in delete`);
         console.log('Error getting list', error);
@@ -25,8 +25,8 @@ function* deleteCurrentGame(action) {
 
 
 function* fetchSaga() {
-    yield takeLatest('GET_GAMES', fetchList);
-    yield takeLatest('DELETE_CURRENT', deleteCurrentGame);
+    yield takeEvery('FETCH_CURRENT', fetchList);
+    yield takeEvery('DELETE_CURRENT', deleteCurrentGame);
     
 }
 

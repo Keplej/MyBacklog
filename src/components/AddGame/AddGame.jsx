56d592769bc781e13a0d562b55e8a0763322b349
Johnny Keple <Johnny.Keple@gmail.com>
2021-06-04 12:02:46 -0500

    Home page stlying

--- src/components/AddGame/AddGame.jsx
+++ src/components/AddGame/AddGame.jsx
@@ -1,7 +1,19 @@
-import { Button, TextField } from '@material-ui/core';
+import { Button, FormControl, InputLabel, MenuItem, Select, TextField } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { useDispatch, useSelector } from 'react-redux';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+
+const useStyles = makeStyles((theme) => ({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  }));
 
 function AddGame() {
     
@@ -11,6 +23,7 @@ function AddGame() {
     const [description, setDescription] = useState('');
     // const [url, setUrl] = useState('');
     const [status, setStatus] = useState(0);
+    const classes = useStyles();
     
     // const status = useSelector((store) => store.status);
     
@@ -53,6 +66,21 @@ function AddGame() {
                 />
                 
                 <br />
+                {/* <FormControl variant="filled" className={classes.formControl}>
+                    <InputLabel>
+                    <Select value={status.id}
+                            name='status'
+                            onChange={(event) => setStatus(event.target.value)}>
+                                {getStatus.map((game) => {
+                        return (
+                        <MenuItem key={game.id} value={game.id}>
+                            {game.name}
+                        </MenuItem>
+                          )
+                        })}
+                    </Select>
+                    </InputLabel>
+                </FormControl> */}
                 <select 
                 value={status.id}
                 name='status'
@@ -73,3 +101,21 @@ function AddGame() {
 }
 
 export default AddGame;
+
+
+// <FormControl variant="filled" className={classes.formControl}>
+//         <InputLabel id="demo-simple-select-filled-label">Age</InputLabel>
+//         <Select
+//           labelId="demo-simple-select-filled-label"
+//           id="demo-simple-select-filled"
+//           value={age}
+//           onChange={handleChange}
+//         >
+//           <MenuItem value="">
+//             <em>None</em>
+//           </MenuItem>
+//           <MenuItem value={10}>Ten</MenuItem>
+//           <MenuItem value={20}>Twenty</MenuItem>
+//           <MenuItem value={30}>Thirty</MenuItem>
+//         </Select>
+//       </FormControl>
---
+++ src/components/UserPageForm/UserPageForm.jsx
@@ -0,0 +1,249 @@
+import { AppBar, Button, Card, CardActions, CardContent, CardHeader, Container, CssBaseline, Grid, Link, Toolbar, Typography } from "@material-ui/core";
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router";
+
+
+
+const useStyles = makeStyles((theme) => ({
+    '@global': {
+      ul: {
+        margin: 0,
+        padding: 0,
+        listStyle: 'none',
+      },
+    },
+    appBar: {
+      borderBottom: `1px solid ${theme.palette.divider}`,
+    },
+    toolbar: {
+      flexWrap: 'wrap',
+    },
+    toolbarTitle: {
+      flexGrow: 1,
+    },
+    link: {
+      margin: theme.spacing(1, 1.5),
+    },
+    heroContent: {
+      padding: theme.spacing(8, 0, 6),
+    },
+    cardHeader: {
+      backgroundColor:
+        theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[700],
+    },
+    cardPricing: {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'baseline',
+      marginBottom: theme.spacing(2),
+    },
+    footer: {
+      borderTop: `1px solid ${theme.palette.divider}`,
+      marginTop: theme.spacing(8),
+      paddingTop: theme.spacing(3),
+      paddingBottom: theme.spacing(3),
+      [theme.breakpoints.up('sm')]: {
+        paddingTop: theme.spacing(6),
+        paddingBottom: theme.spacing(6),
+    },
+    card: {
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+      },
+    },
+  }));
+
+  const current = [
+    {
+          title: 'Currently Playing',
+          description: ['Here we can go to our Currently playing list'],
+          buttonText: 'To Currently Playing',
+          buttonVariant: ['outlined']
+        },
+  ]
+
+
+  const footers = [
+    // {
+    //   title: 'Company',
+    //   description: ['Team', 'History', 'Contact us', 'Locations'],
+    // },
+    // {
+    //   title: 'Features',
+    //   description: ['Cool stuff', 'Random feature', 'Team feature', 'Developer stuff', 'Another one'],
+    // },
+    // {
+    //   title: 'Resources',
+    //   description: ['Resource', 'Resource name', 'Another resource', 'Final resource'],
+    // },
+    // {
+    //   title: 'Legal',
+    //   description: ['Privacy policy', 'Terms of use'],
+    // },
+  ];
+
+function UserPageForm() {
+    const classes = useStyles();
+    const user = useSelector((store) => store.user);
+    const history = useHistory();
+
+
+    return(
+        <React.Fragment>
+        <CssBaseline />
+        <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
+          <Toolbar className={classes.toolbar}>
+            <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
+                Welcome, {user.username}!
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Container maxWidth="sm" component="main" className={classes.heroContent}>
+          <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+            My Backlog Profile {user.username}
+          </Typography>
+          <Typography variant="h5" align="center" color="textSecondary" component="p">
+            This is your dashboard, Here we can navigate to your different lists {user.username}!
+          </Typography>
+        </Container>
+        <Container maxWidth="md" component="main">
+          <Grid container spacing={5} alignItems="flex-end">
+              <Grid md={4}> 
+                <Card>
+                  <CardHeader
+                    title='Completed'
+                    titleTypographyProps={{ align: 'center' }}
+                    subheaderTypographyProps={{ align: 'center' }}
+                    className={classes.cardHeader}
+                  />
+                  <CardContent>
+                    <div className={classes.cardPricing}>
+                      <Typography component="h2" variant="h3" color="textPrimary">
+                      </Typography>
+                      <Typography variant="h6" color="textSecondary">
+                      </Typography>
+                    </div>
+                    <ul>
+                        <Typography 
+                        component="li" 
+                        variant="subtitle1" 
+                        align="center" > 
+                        Here we can go to our Completed list
+                        </Typography>
+                    </ul>
+                  </CardContent>
+                  <CardActions>
+                    <Button 
+                        fullWidth variant='outlined'
+                        color="primary"
+                        onClick={() => {history.push('/current')}}>
+                    Completed
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+          </Grid>
+        </Container>
+        <Container maxWidth="md" component="main">
+          <Grid container spacing={5} alignItems="flex-end">
+              <Grid  xs={12} md={4}>
+                <Card>
+                  <CardHeader
+                    title='Backlog'
+                    titleTypographyProps={{ align: 'center' }}
+                    subheaderTypographyProps={{ align: 'center' }}
+                    className={classes.cardHeader}
+                  />
+                  <CardContent>
+                    <div className={classes.cardPricing}>
+                      <Typography component="h2" variant="h3" color="textPrimary">
+                      </Typography>
+                    </div>
+                    <ul>
+                        <Typography component="li" variant="subtitle1" align="center">
+                        Here we can go to our Backlog list
+                        </Typography>
+                    </ul>
+                  </CardContent>
+                  <CardActions>
+                    <Button  
+                    color="primary"
+                    fullWidth variant='outlined' 
+                    onClick={() => {history.push('/backlog')}}>
+                        Backlog
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+          </Grid>
+        </Container>
+        <Container maxWidth="md" component="main">
+          <Grid container spacing={5} alignItems="flex-end">
+            
+              <Grid i xs={12} sm={12} md={4}>
+                <Card>
+                  <CardHeader
+                    title="Current"
+                    titleTypographyProps={{ align: 'center' }}
+                    subheaderTypographyProps={{ align: 'center' }}
+                    className={classes.cardHeader}
+                  />
+                  <CardContent>
+                    <div className={classes.cardPricing}>
+                      <Typography component="h2" variant="h3" color="textPrimary">
+
+                      </Typography>
+                      <Typography variant="h6" color="textSecondary">
+                        
+                      </Typography>
+                    </div>
+                    <ul>
+
+                        <Typography component="li" variant="subtitle1" align="center">
+                        Here we can go to our Current list
+                        </Typography>
+
+                    </ul>
+                  </CardContent>
+                  <CardActions>
+                    <Button fullWidth variant='outlined' color="primary" onClick={() => {history.push('/current')}}>
+                        Current
+                    </Button>
+                  </CardActions>
+                </Card>
+              </Grid>
+          </Grid>
+        </Container>
+
+        
+        
+
+        <Container maxWidth="md" component="footer" className={classes.footer}>
+          <Grid container spacing={4} justify="space-evenly">
+            {footers.map((footer) => (
+              <Grid item xs={6} sm={3} key={footer.title}>
+                <Typography variant="h6" color="textPrimary" gutterBottom>
+                  {footer.title}
+                </Typography>
+                <ul>
+                  {footer.description.map((item) => (
+                    <li key={item}>
+                      <Link href="#" variant="subtitle1" color="textSecondary">
+                        {item}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </Grid>
+            ))}
+          </Grid>
+        </Container>
+        {/* End footer */}
+      </React.Fragment>
+    )
+}
+
+export default UserPageForm;
